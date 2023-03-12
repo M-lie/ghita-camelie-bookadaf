8aad35f44de1056367635e861d2cb8d31b6353e2
M-lie <camelielaprise@hotmail.com>
2023-03-12 19:48:54 -0400

    p.1.3.3_app

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper-filters', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 'auto',
+
+});
+
+const swiper = new Swiper('.swiper-featured-artworks', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: '2',
+
+});
