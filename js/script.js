5180459904eac17794c192e2192e9f394fc94894
M-lie <camelielaprise@hotmail.com>
2023-03-12 21:02:33 -0400

    p.1.3.3_app_p.1.2_corrections

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
+function changeColor(btn) {
+    btn.style.backgroundColor = "#2f8d8e";
+}
+
+
 const swiper = new Swiper('.swiper-filters', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
 
     slidesPerView: 'auto',
-
+    spaceBetween: 1,
 });
 
-const swiper = new Swiper('.swiper-featured-artworks', {
+swiper = new Swiper('.swiper-featured-artworks', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
 
-    slidesPerView: '2',
-
+    slidesPerView: 2.3,
 });
