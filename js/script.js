8845820234abb1839e3461bcb40188aa8f5fdcb5
M-lie <camelielaprise@hotmail.com>
2023-03-14 22:56:35 -0400

    p.1.3.3_lp

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function changeColor(btn) {
   btn.style.backgroundColor = "#2f8d8e";
 }
 
-const swiper = new Swiper(".swiper-filters", {
+const swiper1 = new Swiper(".swiper-filters", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
@@ -12,7 +12,7 @@ const swiper = new Swiper(".swiper-filters", {
   spaceBetween: 1,
 });
 
-swiper = new Swiper(".swiper-featured-artworks", {
+const swiper2 = new Swiper(".swiper-featured-artworks", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
@@ -21,23 +21,24 @@ swiper = new Swiper(".swiper-featured-artworks", {
 });
 
 // Landing page
-
-const swiperlp = new Swiper(".swiper-screenshots", {
+const swiper3 = new Swiper(".swiper-screenshots", {
   direction: "horizontal",
   loop: true,
+  centeredSlides: true,
 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  // Default parameters
-  slidesPerView: 1,
 
-  // Breakpoints:
+  slidesPerView: 1,
+  spaceBetween: 20,
+
   breakpoints: {
-    // when window width is >= 1200px
     1200: {
       slidesPerView: 3,
-    },
-  },
+      spaceBetween: 20
+    }
+  }
 });
+
